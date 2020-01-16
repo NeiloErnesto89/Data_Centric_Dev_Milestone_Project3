92cddf9df1491a970ea800a61c19acb56fedc771
Ubuntu <ubuntu@ip-172-31-37-91.ec2.internal>
2020-01-16 17:04:10 +0000

    fixed all the untracked files

--- env.py
+++
@@ -1,5 +0,0 @@
-import os 
-
-#os.environ["MONGODB_NAME"] = "book_reviews"
-os.environ["MONGO_URI"]= 'mongodb+srv://foobar:fooboo123@mynewcluster-fo6gq.mongodb.net/book_reviews?retryWrites=true&w=majority'
-
